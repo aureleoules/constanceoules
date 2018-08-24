18dfc597961af2dfa7f6fd07f96ca904e8da2d68
Aurèle Oulès <aurele@oules.com>
2018-08-24 20:53:17 +0200

    renamed homepage 'project' to 'Design'

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -17,282 +17,282 @@ class Home extends React.Component {
 				<div className="photos">
 					<div className="row">
 						<div className="two third">
-							<img alt="project" src={this.getPicture(1)}/>
+							<img alt="Design" src={this.getPicture(1)}/>
 						</div>
 						<div className="third">
 							<div className="top">
-								<img alt="project" src={this.getPicture(2)}/>
+								<img alt="Design" src={this.getPicture(2)}/>
 							</div>
 							<div className="bottom">
-								<img alt="project" src={this.getPicture(3)}/>
+								<img alt="Design" src={this.getPicture(3)}/>
 							</div>
 						</div>
 					</div>
 					<div className="row">
 						<div className="third">
-							<img alt="project" src={this.getPicture(4)}/>
+							<img alt="Design" src={this.getPicture(4)}/>
 						</div>
 						<div className="two third">
-							<img alt="project" src={this.getPicture(5)}/>
+							<img alt="Design" src={this.getPicture(5)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="half">
-							<img alt="project" src={this.getPicture(6)}/>
+							<img alt="Design" src={this.getPicture(6)}/>
 						</div>
 						<div className="half">
-							<img alt="project" src={this.getPicture(7)}/>
+							<img alt="Design" src={this.getPicture(7)}/>
 						</div>
 					</div>
 					<div className="row three">
 						<div className="third">
-							<img alt="project" src={this.getPicture(8)}/>
+							<img alt="Design" src={this.getPicture(8)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(9)}/>
+							<img alt="Design" src={this.getPicture(9)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(10)}/>
+							<img alt="Design" src={this.getPicture(10)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="third">
 							<div className="top">
-								<img alt="project" src={this.getPicture(11)}/>
+								<img alt="Design" src={this.getPicture(11)}/>
 							</div>
 							<div className="bottom">
-								<img alt="project" src={this.getPicture(12)}/>
+								<img alt="Design" src={this.getPicture(12)}/>
 							</div>
 						</div>
 						<div className="two third">
-							<img alt="project" src={this.getPicture(13)}/>
+							<img alt="Design" src={this.getPicture(13)}/>
 						</div>
 					</div>
 					<div className="row three">
 						<div className="third">
-							<img alt="project" src={this.getPicture(14)}/>
+							<img alt="Design" src={this.getPicture(14)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(15)}/>
+							<img alt="Design" src={this.getPicture(15)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(16)}/>
+							<img alt="Design" src={this.getPicture(16)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="half">
-							<img alt="project" src={this.getPicture(17)}/>
+							<img alt="Design" src={this.getPicture(17)}/>
 						</div>
 						<div className="half">
-							<img alt="project" src={this.getPicture(18)}/>
+							<img alt="Design" src={this.getPicture(18)}/>
 						</div>
 					</div>
 					<div className="row three">
 						<div className="third">
-							<img alt="project" src={this.getPicture(19)}/>
+							<img alt="Design" src={this.getPicture(19)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(20)}/>
+							<img alt="Design" src={this.getPicture(20)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(21)}/>
+							<img alt="Design" src={this.getPicture(21)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="two third">
-							<img alt="project" src={this.getPicture(22)}/>
+							<img alt="Design" src={this.getPicture(22)}/>
 						</div>
 						<div className="third">
 							<div className="top">
-								<img alt="project" src={this.getPicture(23)}/>
+								<img alt="Design" src={this.getPicture(23)}/>
 							</div>
 							<div className="bottom">
-								<img alt="project" src={this.getPicture(24)}/>
+								<img alt="Design" src={this.getPicture(24)}/>
 							</div>
 						</div>
 					</div>
 					<div className="row">
 						<div className="half">
-							<img alt="project" src={this.getPicture(25)}/>
+							<img alt="Design" src={this.getPicture(25)}/>
 						</div>
 						<div className="half">
-							<img alt="project" src={this.getPicture(26)}/>
+							<img alt="Design" src={this.getPicture(26)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="third">
-							<img alt="project" src={this.getPicture(27)}/>
+							<img alt="Design" src={this.getPicture(27)}/>
 						</div>
 						<div className="two third">
-							<img alt="project" src={this.getPicture(28)}/>
+							<img alt="Design" src={this.getPicture(28)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="half">
-							<img alt="project" src={this.getPicture(29)}/>
+							<img alt="Design" src={this.getPicture(29)}/>
 						</div>
 						<div className="half">
-							<img alt="project" src={this.getPicture(30)}/>
+							<img alt="Design" src={this.getPicture(30)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="two third">
-							<img alt="project" src={this.getPicture(31)}/>
+							<img alt="Design" src={this.getPicture(31)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(32)}/>
+							<img alt="Design" src={this.getPicture(32)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="third">
 							<div className="top">
-								<img alt="project" src={this.getPicture(33)}/>
+								<img alt="Design" src={this.getPicture(33)}/>
 							</div>
 							<div className="bottom">
-								<img alt="project" src={this.getPicture(34)}/>
+								<img alt="Design" src={this.getPicture(34)}/>
 							</div>
 						</div>
 						<div className="two third">
-							<img alt="project" src={this.getPicture(35)}/>
+							<img alt="Design" src={this.getPicture(35)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="half">
-							<img alt="project" src={this.getPicture(36)}/>
+							<img alt="Design" src={this.getPicture(36)}/>
 						</div>
 						<div className="half">
-							<img alt="project" src={this.getPicture(37)}/>
+							<img alt="Design" src={this.getPicture(37)}/>
 						</div>
 					</div>
 					<div className="row three">
 						<div className="third">
-							<img alt="project" src={this.getPicture(38)}/>
+							<img alt="Design" src={this.getPicture(38)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(39)}/>
+							<img alt="Design" src={this.getPicture(39)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(40)}/>
+							<img alt="Design" src={this.getPicture(40)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="two third">
-							<img alt="project" src={this.getPicture(41)}/>
+							<img alt="Design" src={this.getPicture(41)}/>
 						</div>
 						<div className="third">
 							<div className="top">
-								<img alt="project" src={this.getPicture(42)}/>
+								<img alt="Design" src={this.getPicture(42)}/>
 							</div>
 							<div className="bottom">
-								<img alt="project" src={this.getPicture(43)}/>
+								<img alt="Design" src={this.getPicture(43)}/>
 							</div>
 						</div>
 					</div>
 					<div className="row three">
 						<div className="third">
-							<img alt="project" src={this.getPicture(44)}/>
+							<img alt="Design" src={this.getPicture(44)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(45)}/>
+							<img alt="Design" src={this.getPicture(45)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(46)}/>
+							<img alt="Design" src={this.getPicture(46)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="half">
-							<img alt="project" src={this.getPicture(47)}/>
+							<img alt="Design" src={this.getPicture(47)}/>
 						</div>
 						<div className="half">
-							<img alt="project" src={this.getPicture(48)}/>
+							<img alt="Design" src={this.getPicture(48)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="third">
 							<div className="top">
-								<img alt="project" src={this.getPicture(49)}/>
+								<img alt="Design" src={this.getPicture(49)}/>
 							</div>
 							<div className="bottom">
-								<img alt="project" src={this.getPicture(50)}/>
+								<img alt="Design" src={this.getPicture(50)}/>
 							</div>
 						</div>
 						<div className="two third">
-							<img alt="project" src={this.getPicture(51)}/>
+							<img alt="Design" src={this.getPicture(51)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="two third">
-							<img alt="project" src={this.getPicture(52)}/>
+							<img alt="Design" src={this.getPicture(52)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(53)}/>
+							<img alt="Design" src={this.getPicture(53)}/>
 						</div>
 					</div>
 					<div className="row three">
 						<div className="third">
-							<img alt="project" src={this.getPicture(54)}/>
+							<img alt="Design" src={this.getPicture(54)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(55)}/>
+							<img alt="Design" src={this.getPicture(55)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(56)}/>
+							<img alt="Design" src={this.getPicture(56)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="third">
 							<div className="top">
-								<img alt="project" src={this.getPicture(57)}/>
+								<img alt="Design" src={this.getPicture(57)}/>
 							</div>
 							<div className="bottom">
-								<img alt="project" src={this.getPicture(58)}/>
+								<img alt="Design" src={this.getPicture(58)}/>
 							</div>
 						</div>
 						<div className="two third">
-							<img alt="project" src={this.getPicture(59)}/>
+							<img alt="Design" src={this.getPicture(59)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="two third">
-							<img alt="project" src={this.getPicture(60)}/>
+							<img alt="Design" src={this.getPicture(60)}/>
 						</div>
 						<div class="third">
-							<img alt="project" src={this.getPicture(61)}/>
+							<img alt="Design" src={this.getPicture(61)}/>
 						</div>
 					</div>
 					<div className="row three">
 						<div className="third">
-							<img alt="project" src={this.getPicture(62)}/>
+							<img alt="Design" src={this.getPicture(62)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(63)}/>
+							<img alt="Design" src={this.getPicture(63)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(64)}/>
+							<img alt="Design" src={this.getPicture(64)}/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="two third">
-							<img alt="project" src={this.getPicture(65)}/>
+							<img alt="Design" src={this.getPicture(65)}/>
 						</div>
 						<div className="third">
 							<div className="top">
-								<img alt="project" src={this.getPicture(66)}/>
+								<img alt="Design" src={this.getPicture(66)}/>
 							</div>
 							<div className="bottom">
-								<img alt="project" src={this.getPicture(67)}/>
+								<img alt="Design" src={this.getPicture(67)}/>
 							</div>
 						</div>
 					</div>
 					<div className="row three">
 						<div className="third">
-							<img alt="project" src={this.getPicture(68)}/>
+							<img alt="Design" src={this.getPicture(68)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(69)}/>
+							<img alt="Design" src={this.getPicture(69)}/>
 						</div>
 						<div className="third">
-							<img alt="project" src={this.getPicture(70)}/>
+							<img alt="Design" src={this.getPicture(70)}/>
 						</div>
 					</div>
 				</div>
