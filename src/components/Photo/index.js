05a41068d6ef509e921d1556c3179ab320c37e0f
unknown <aurele@oules.com>
2019-10-18 19:32:44 +0200

    fixed zoom

--- src/components/Photo/index.js
+++ src/components/Photo/index.js
@@ -8,8 +8,40 @@ class Photo extends React.Component {
     onPhotoClick = e => {
         if(this.props.zoom && e.target.id !== "picture") {
             this.props.removeZoom();
+            this.setState({
+                opacity: 0
+            })
         } else {
+            
+            
             this.props.onClick();
+            
+            const check = () => {
+                setTimeout(() => {
+                    if(!this.pictureRef) return check();
+                    if(!this.pictureRef.naturalWidth || !this.pictureRef.naturalHeight) return check();
+                    const width = 0.85 * (this.pictureRef.naturalWidth * (this.pictureRef.naturalWidth / this.pictureRef.naturalHeight));
+                    this.setState({
+                        currentHeight: window.innerWidth >= 900 ? 85 : null, 
+                        currentWidth: window.innerWidth >= 900 ? width : null,
+                        maxWidth: "unset",
+                        opacity: 1
+                    })
+                }, 10);
+            }
+            
+            check();
+
+
+        }
+    }
+
+    constructor(props) {
+        super(props);
+        this.state ={
+            width: 0,
+            height: 0,
+            maxWidth: "30"
         }
     }
 
@@ -25,12 +57,14 @@ class Photo extends React.Component {
                     this.props.fade ? "fade":"",
                     this.props.zoom ? "zoom": ""
                 ].join(" ")}>
-                <div className="img-container">
-                    {this.props.zoom && <a className="cross">
-                        <img alt="cross" src={require("../../assets/icons/cross" + (this.props.isMobile ? "_mobile" : "") + ".svg")}/>
+                <div className="img-container" style={this.props.zoom ? `display: ${this.state.display}; width: ${this.state.currentWidth}px!important; height: ${this.state.currentHeight}vh!important; max-width: unset;`: ""}>
+                    {this.props.zoom && this.state.opacity && <a className="cross">
+                        <img style="opacity: 1;" alt="cross" src={require("../../assets/icons/cross" + (this.props.isMobile ? "_mobile" : "") + ".svg")}/>
                     </a>}
-                    {this.props.zoom && <img 
-                        id="picture" 
+                    {this.props.zoom && <img
+                        ref={pic => this.pictureRef = pic}
+                        style={`max-width: unset!important; opacity: ${this.state.opacity};`}
+                        id="picturezoomed" 
                         alt={this.props.title || this.props.alt} 
                         src={highQuality(this.props.src)}/>}
                     {!this.props.zoom && <img 
